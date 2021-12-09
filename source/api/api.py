90bc875960c81ac7eeef324088dd27d2d0c273d6
an aspirin <beol0127@gmail.com>
2021-12-09 15:15:22 -0500

    chore: update api

--- source/api/api.py
+++ source/api/api.py
@@ -1,1 +1,12 @@
+from flask import Flask, request, render_template # Add more functions to import
+import os
+import json
+import requests
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return "IoAPI"
+
+app.run(host="0.0.0.0", port=8080)
