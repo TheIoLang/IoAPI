b1055b7f23934d22daa85e5e7780583a19df2044
an aspirin <beol0127@gmail.com>
2021-12-11 00:48:52 +0000

    chore: update the api

--- source/api/api.py
+++ source/api/api.py
@@ -1,10 +1,24 @@
-from flask import Flask, request, render_template # Add more functions to import
+from flask import Flask, render_template, send_from_directory, url_for, request, redirect
 import os
 import json
 import requests
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='', 
+            static_folder='static',
+            template_folder='templates')
 
 @app.route('/')
 def index():
   return "IoAPI"
+
+@app.route('/api')
+def api_route():
+  syntax = request.args.get('syntax')
+  visualizer = request.args.get('visualizer')
+  syntax = syntax.lower()
+
+  if syntax == "true" or syntax == "yes":
+    return render_template("syntax.html")
+  elif visualizer == "true" or syntax == "yes":
+    return render_template("visualizer.html")
---
+++ source/api/static/index.js
@@ -0,0 +1,1 @@
+console.log("Io's API");
